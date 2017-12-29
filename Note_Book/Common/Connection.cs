82c9597a3b009a92ba444ee2b93851a43881b44f
ajaxGX <m15735042025@163.com>
2017-12-29 10:26:36 +0800

    h

---
+++ Note_Book/Common/Connection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace Common
+{
+    public class Connection
+    {
+      
+
+        public static  MySqlConnection sqlConn() {
+            string constr = System.Configuration.ConfigurationSettings.AppSettings["MyConnectionStringModel"];
+            MySqlConnection connection = new MySqlConnection(constr);
+            connection.Open();
+            return connection;
+        }
+        static void Main(string[] args)
+        {
+
+
+            //string constr = "server=127.0.0.1;User Id=root;password=root;Database=text";
+            MySqlConnection connection = sqlConn();
+            //connection.Open();
+
+            string sql = "insert into user values ('jamy','imjamy','n','xxxxxxxx')";
+            MySqlCommand scd = new MySqlCommand(sql, connection);
+            scd.ExecuteNonQuery();
+            connection.Close();
+
+            //String sql = "select distinct * from text where user = 'tom'";
+            //MySqlCommand scd = new MySqlCommand(sql, connection);
+            //MySqlDataReader result = scd.ExecuteReader();
+            //while (result.Read())
+            //{
+
+            //    Console.WriteLine((string)result[1]);
+            //}
+
+        }
+    }
+}
