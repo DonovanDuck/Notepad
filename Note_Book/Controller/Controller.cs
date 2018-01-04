4ef2650809b3b65155f3f46a8b112ce8a5731649
ajaxGX <m15735042025@163.com>
2018-01-04 20:26:25 +0800

    h

---
+++ Note_Book/Controller/Controller.cs
@@ -0,0 +1,152 @@
+﻿using Model_Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Text_model;
+using Text_Dao;
+using Method_impl;
+
+namespace Controller
+{
+    public class Controller
+    {
+        
+        public void newText( FunctionEvent e)
+        {
+            if ("1".Equals(e.Choose))
+            {
+                e.text = new Text();
+                e.th.write(e.text);
+                if (e.user == null)
+                {
+                    Console.WriteLine("请先登录");
+                }
+                else
+                {
+                    if (e.dao != null)
+                    {
+                        e.text.User = e.user.Username;
+                        TextDbHandlerImp.insertEvent += e.dao.saveText;
+                    }
+                    e.th.SaveFile(e.text);
+                }
+            }
+        }
+
+        public void opentext(FunctionEvent e) {
+            if ("2".Equals(e.Choose)) {
+                e.text = new Text();
+                if (e.user != null)
+                {
+                    if (e.dao != null)
+                    {
+                        TextDbHandlerImp.searchtextEvent += e.dao.openText;
+                        e.text = e.thdb.OpenFile(e.user.Username);
+                    }
+                    else
+                    {
+                        e.text = e.th.OpenFile();
+                    }
+                    Console.WriteLine("笔记内容：\n" + e.text.Content);
+                }
+                else
+                {
+                    Console.WriteLine("请先登录");
+                }
+            }
+        }
+
+        public void saveText(FunctionEvent e) {
+            if ("3".Equals(e.Choose)) {
+                if (e.user != null)
+                {
+                    if (e.text.Content == null || "".Equals(e.text.Content))
+                    {
+                        Console.WriteLine("请先新建笔记");
+                    }
+                    else
+                    {
+                        TextDbHandlerImp.insertEvent += e.dao.saveText;
+                        e.th.SaveFile(e.text);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("请先登录");
+                }
+            }
+        }
+        public void login( FunctionEvent e)
+        {
+            if ("4".Equals(e.Choose))
+            {
+                TextDbHandlerImp.searchuserEvent += e.dao.signIn;
+                e.user = e.thdb.signIn();
+                if (e.user != null)
+                {
+                    Console.WriteLine("欢迎：" + e.user.Username + "，请继续操作");
+                    if (e.text != null)
+                    {
+                        e.text.User = e.user.Username;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("用户名或密码错误");
+
+                }
+            }
+        }
+        public void register( FunctionEvent e)
+        {
+            if ("5".Equals(e.Choose))
+            {
+                TextDbHandlerImp.insertEvent += e.dao.signUp;
+                e.thdb.signUp();
+            }
+        }
+
+        public void exit( FunctionEvent e)
+        {
+            if ("0".Equals(e.Choose))
+            {
+                return;
+            }
+        }
+    }
+
+
+    public class FunctionEvent {
+        private  string _choose;
+        public Text text;
+        public  TextHandle th;
+        public TextDbHandlerImp thdb;
+        public User user;
+        public Text_Dao.Dao dao;
+
+        public FunctionEvent(Text text, TextHandle th, TextDbHandlerImp thdb, User user, Text_Dao.Dao dao)
+        {
+            this.text = text;
+            this.th = th;
+            this.thdb = thdb;
+            this.user = user;
+            this.dao = dao;
+        }
+
+        public string Choose
+        {
+            get
+            {
+                return _choose;
+            }
+
+            set
+            {
+                _choose = value;
+            }
+        }
+       
+    }
+}
--- Note_Book/Note_Book/Program.cs
+++ Note_Book/Note_Book/Program.cs
@@ -9,7 +9,8 @@ using Method_impl;
 using Model_Interface;
 using System.IO;
 using System.Windows.Forms;
-
+using Controller;
+using Note_Book;
 
 namespace Note_Book
 {
@@ -20,11 +21,23 @@ namespace Note_Book
         private static TextDbHandlerImp thdb; 
         private static User user;
         private static Text_Dao.Dao dao;
-
-
-        static void Main(string[] args)
-        {
+        public delegate void MenuEvents( FunctionEvent e);
+        public static event MenuEvents menuevnts;
+        public static Controller.Controller con;
+       static public  void menu(Text text, TextHandle th, TextDbHandlerImp thdb, User user, Text_Dao.Dao dao, FunctionEvent e) {
             
+            th.showMenu();
+            string s = Console.ReadLine();
+            e.Choose = s;
+            onFunction(menuevnts, e);
+        }
+
+       protected static void onFunction(MenuEvents menuevnts, FunctionEvent e) {
+            menuevnts?.Invoke(e);
+        }
+
+       static void Main(string[] args)
+        {
             Console.WriteLine("请选择版本：1.0 / 1.1");
             string c = Console.ReadLine();
             if ("1.0".Equals(c))
@@ -32,106 +45,24 @@ namespace Note_Book
                 user = new User();
                 th = new TextImpl();
             }
-            else {
+            else
+            {
                 dao = new Text_Dao.Dao();
                 th = new TextDbHandlerImp();
                 thdb = new TextDbHandlerImp();
             }
-            
+            con = new Controller.Controller();
+            FunctionEvent e = new FunctionEvent(text, th, thdb, user, dao);
+            menuevnts += con.newText;
+            menuevnts += con.opentext;
+            menuevnts += con.saveText;
+            menuevnts += con.login;
+            menuevnts += con.register;
+            menuevnts += con.exit;
             while (true)
             {
-                th.showMenu();
-                string s = Console.ReadLine();
-
-                switch (s)
-                {
-                    case "1":
-                        {
-                            text = new Text();
-                            th.write(text);
-                            if (user == null)
-                            {
-                                Console.WriteLine("请先登录");
-                            }
-                            else
-                            {
-                                if (dao!=null) {
-                                    text.User = user.Username;
-                                    TextDbHandlerImp.insertEvent += dao.saveText;
-                                }
-                                th.SaveFile(text);
-                            }
-                            break;
-                        }
-                    case "2":
-                        {
-                            text = new Text();
-                            if (user!=null) {
-                                if (dao != null)
-                                {
-                                    TextDbHandlerImp.searchtextEvent += dao.openText;
-                                    text = thdb.OpenFile(user.Username);
-                                }
-                                else {
-                                    text = th.OpenFile();
-                                }
-                                Console.WriteLine("笔记内容：\n" + text.Content);
-                            }
-                            else {
-                                Console.WriteLine("请先登录");
-                            }
-                            break;
-                        }
-                    case "3":
-                        {
-                            if (user != null) {
-                                if (text.Content == null || "".Equals(text.Content)) {
-                                    Console.WriteLine("请先新建笔记");
-                                }
-                                else {
-                                    TextDbHandlerImp.insertEvent += dao.saveText;
-                                    th.SaveFile(text);
-                                }    
-                            }
-                            else {
-                                Console.WriteLine("请先登录");
-                            }
-                            
-                            break;
-                        }
-                    case "4": {
-                            TextDbHandlerImp.searchuserEvent += dao.signIn;
-                            user =  thdb.signIn();
-                            if (user != null)
-                            {
-                                Console.WriteLine("欢迎：" + user.Username + "，请继续操作");
-                                if (text != null)
-                                {
-                                    text.User = user.Username;
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("用户名或密码错误");
-                                
-                            }
-
-                            break;
-                        }
-                    case "5":
-                        {
-                            TextDbHandlerImp.insertEvent += dao.signUp;
-                            thdb.signUp();
-                            break;
-                        }
-                    case "0":
-                        {
-                            return;
-                        }
-                }
+                menu(text, th, thdb, user, dao,e);
             }
-
-
         }
     }
 }
